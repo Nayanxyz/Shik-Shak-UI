7eac917a2af69b5b26b85d7db9abcc3f98724601
NayanPython <choudharyji527@gmail.com>
2026-06-27 11:11:16 +0530

    removed localhost

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from 'socket.io-client'
 
-const SOCKET_URL = import.meta.env.VITE_API_URL || 'http://localhost:5500'
+const SOCKET_URL = import.meta.env.VITE_API_URL 
 
 let socket: Socket | null = null
 
