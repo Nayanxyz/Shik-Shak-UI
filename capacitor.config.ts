d0d1b00cae14e2bfca7beea9b699a5e5ef78622f
NayanPython <choudharyji527@gmail.com>
2026-06-29 15:47:39 +0530

    added

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.shikshak.app',
+  appName: 'ShikShak',
+  webDir: 'dist'
+};
+
+export default config;
